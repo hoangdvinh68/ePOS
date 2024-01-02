73acd6a429426b4b649b2921b2073916195912ab
toilavinh <hoangdvinh68@gmail.com>
2024-01-02 10:54:14 +0700

    setup primeng & tailwind not conflict core plugin

--- src/client/ePOS.Client/src/app/app.component.ts
+++ src/client/ePOS.Client/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private _primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this._primengConfig.ripple = true;
+    this._primengConfig.zIndex = {
+      modal: 1100, // dialog, sidebar
+      overlay: 1000, // dropdown, overlay panel
+      menu: 1000, // overlay menus
+      tooltip: 1100, // tooltip
+    };
+  }
+}
